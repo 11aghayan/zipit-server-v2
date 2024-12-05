89c6c8d9d48815b453fbd0d5ba0ea2b9c3288392
11aghayan <11.aghayan.99@gmail.com>
2024-12-06 00:56:47 +0400

    logout set up

--- src/controllers/auth-controllers.ts
+++ src/controllers/auth-controllers.ts
@@ -5,6 +5,7 @@ import { T_Controller } from "../types";
 import { custom_error, server_error } from "../util/error_handlers";
 
 const { JWT_REFRESH_TOKEN_SECRET, JWT_ACCESS_TOKEN_SECRET } = process.env as { [key: string]: string };
+const COOKIE_MAX_AGE = 24 * 60 * 60 * 1000;
 
 export const login: T_Controller = async function(req, res) {
   const { username } = req.body;
@@ -16,8 +17,7 @@ export const login: T_Controller = async function(req, res) {
     const db_response = await  Db.update_refresh_token(refresh_token);
     if (db_response instanceof Db.Db_Error_Response) return custom_error(res, 400, "Refresh token updating error");
     
-    const MAX_AGE = 24 * 60 * 60 * 1000;
-    res.cookie('jwt', refresh_token, { httpOnly: true, sameSite: 'none', maxAge: MAX_AGE, secure: true });
+    res.cookie('jwt', refresh_token, { httpOnly: true, sameSite: 'none', maxAge: COOKIE_MAX_AGE, secure: true });
     return res.status(200).json({ access_token });
   } catch (error) {
     return server_error(res, "login", error);
@@ -50,7 +50,23 @@ export const refresh_token: T_Controller = async function(req, res) {
 }
 
 export const logout: T_Controller = async function(req, res) {
+  const { jwt: refresh_token } = req.cookies;
 
+  if (!refresh_token) return res.sendStatus(200);
+  
+  const user = await Db.get_user_by_refresh_token(refresh_token);
+  if (user instanceof Db.Db_Error_Response) return custom_error(res, 400, "User fetching error");
+
+  if (user.rows.length < 1) {
+    res.clearCookie("jwt", { httpOnly: true, sameSite: "none", secure: true });
+    return res.sendStatus(200);
+  }
+  
+  const response = await Db.update_refresh_token(null);
+  if (response instanceof Db.Db_Error_Response) return custom_error(res, 400, "Refresh token updating error");
+
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "none", secure: true });
+  return res.sendStatus(200);
 }
 
 export const change_password: T_Controller = async function(req, res) {
