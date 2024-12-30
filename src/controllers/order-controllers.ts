f650b45e766cc2264a04f626267e57838c1a272c
11aghayan <11.aghayan.99@gmail.com>
2024-12-30 14:51:32 +0400

    smtp text

--- src/controllers/order-controllers.ts
+++ src/controllers/order-controllers.ts
@@ -1,9 +1,11 @@
 import { createTransport } from "nodemailer";
+import SMTPConnection from "nodemailer/lib/smtp-connection"; 
 
 import * as Db from "../db";
 import { T_Controller } from "../types";
 import { custom_error, server_error } from "../util/error_handlers";
 import { generate_email_message } from "../util/order-utils";
+
 
 const transporter = createTransport({
   host: process.env.NODEMAILER_HOST as string,
@@ -16,7 +18,17 @@ const transporter = createTransport({
   debug: true
 });
 
+
 export const confirm_order: T_Controller = async function(req, res) {
+  const connection = new SMTPConnection({
+    host: process.env.NODEMAILER_HOST as string,
+    port: Number(process.env.NODEMAILER_PORT as string),
+    secure: !!Number(process.env.NODEMAILER_SECURE),
+    auth: {
+      user: process.env.NODEMAILER_USER as string,
+      pass: process.env.NODEMAILER_PASSWORD as string
+    },
+  });
   const { order } = req.body;
   try {
     const items = await Db.get_items_by_photo_ids(Object.keys(order));
@@ -24,14 +36,19 @@ export const confirm_order: T_Controller = async function(req, res) {
       return custom_error(res, 500, "db_error");
     }
     const email_message = generate_email_message({ ...req.body, items: items.rows });
-    const mail_res = await transporter.sendMail(email_message);
-    console.log(mail_res);
-    if (mail_res.rejected.length > 0) {
-      return custom_error(res, 500, "Order error");
-    }
+
+    connection.connect((err) => {
+      if (err) {
+        return custom_error(res, 500, "Order error");
+      }
+      return res.status(200).json({ ok: true });
+    });
     
-    console.log(mail_res);
-    return res.status(200).json({ ok: true });
+    // const mail_res = await transporter.sendMail(email_message);
+    // console.log(mail_res);
+    // if (mail_res.rejected.length > 0) {
+      //   return custom_error(res, 500, "Order error");
+      // }
   } catch (error) {
     return server_error(res, "confirm_order", error);
   }
